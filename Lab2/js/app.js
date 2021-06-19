4fe177243a4bc35efa17db77a75c281a1110f395
Vladislav Petrov <58428903+Vladislav1104@users.noreply.github.com>
2021-06-19 11:02:15 +0300

    add protection

--- Lab2/js/app.js
+++ Lab2/js/app.js
@@ -1,9 +1,11 @@
 let symbols = ['C','1', '2', '3', '+', '4', '5', '6', '-','7', '8', '9', '/', '0', '.', '=', '*'];
-
+const pointAndDigit = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
+const operations = ['+', '-', '/', '*', '.'];
 let calc = document.getElementsByClassName('calc')[0];
-    
+  
 let text = document.createElement('p');
 text.className = 'text_area';
+text.innerHTML = '0';
 calc.appendChild(text);
 
 symbols.forEach(function (symbol) {
@@ -17,8 +19,10 @@ document.querySelectorAll('.btn').forEach(function (button) {
     button.addEventListener('click', onButtonClick);
 });
 
-function onButtonClick(btn_click) {
+
+function onButtonClick(btn_click) { 
     if (btn_click.target.innerHTML === 'C') {
+        console.log(typeof text.innerHTM);
         text.innerHTML = '0';
     } else if (btn_click.target.innerHTML === '=') {
         try{
@@ -28,8 +32,13 @@ function onButtonClick(btn_click) {
             text.innerHTML = 'invalid input';
         }
         
-    } else if (text.innerHTML === '0' && btn_click.target.innerHTML !== '.') {
-        text.innerHTML = btn_click.target.innerHTML;
+    } else if (text.innerHTML === '0' && !(pointAndDigit.includes(btn_click.target.innerHTML))) {
+        text.innerHTML = '0';   
+    } else if (operations.includes(text.innerHTML.slice(-1)) && operations.includes(btn_click.target.innerHTML)) {
+        text.innerHTML.slice(text.innerHTML.length) = btn_click.target.innerHTML;
+    // } else if(text.innerHTML === '0' && btn_click.target.innerHTML.includes('+')){
+    //     console.log(text.innerHTML);
+    //    text.innerHTML = '0';
     } else {
         text.innerHTML += btn_click.target.innerHTML;
     }
